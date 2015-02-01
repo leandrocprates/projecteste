6bc83a25a0f0a69ba779a906306cc47be10cec70
leandro.prates@gmail.com <leandro.prates@gmail.com@7031352b-57be-d200-b497-b01c49db2c4a>
2015-02-01 20:16:13 +0000

    criado exemplo de envio de css e barra de progresso

--- aplicacoes/linguagem_java/SendEmail/src/main/java/com/nancydeschenes/emaildemo/Main.java
+++ aplicacoes/linguagem_java/SendEmail/src/main/java/com/nancydeschenes/emaildemo/Main.java
@@ -43,6 +43,21 @@ public class Main {
     Message withImage = buildMessageWithEmbeddedImage(session);
     addressAndSendMessage(withImage, "leandro.prates@asisprojetos.com.br");
     
+    Message htmlTeste = HtmlTeste(session);
+    addressAndSendMessage(htmlTeste, "leandro.prates@asisprojetos.com.br");
+    
+    Message htmlTeste2 = HtmlTeste2(session);
+    addressAndSendMessage(htmlTeste2, "leandro.prates@asisprojetos.com.br");
+    
+    Message htmlTeste3 = HtmlTeste3(session);
+    addressAndSendMessage(htmlTeste3, "leandro.prates@asisprojetos.com.br");
+    
+    Message htmlTeste4 = HtmlTeste4(session);
+    addressAndSendMessage(htmlTeste4, "leandro.prates@asisprojetos.com.br");
+    
+    
+    
+    
   }
 
   //
@@ -220,6 +235,254 @@ public class Main {
     
   }
 
+  
+  public static Message HtmlTeste(Session session)
+      throws MessagingException {
+      
+    SMTPMessage m = new SMTPMessage(session);
+    MimeMultipart content = new MimeMultipart();
+    MimeBodyPart mainPart = new MimeBodyPart();
+    mainPart.setText(
+    
+"<!DOCTYPE html>"+
+"<html lang=\"en\">"+
+  "<head>"+
+    "<meta charset=\"utf-8\">"+
+    "<meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">"+
+    "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">"+
+    "<title>Bootstrap 101 Template</title>"+
+    "<link rel=\"stylesheet\" href=\"https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css\">"+
+  "</head>"+
+
+  "<body>"+
+    "<h1>Hello, world!</h1>"+
+	"<div class=\"progress\">"+
+	  "<div class=\"progress-bar\" role=\"progressbar\" aria-valuenow=\"60\" aria-valuemin=\"0\" aria-valuemax=\"100\" style=\"width: 60%;\">"+
+	   " 60% "+
+	  "</div>"+
+	"</div>"+
+  "</body>"+
+"</html>    " , "US-ASCII", "html"
+    
+    
+    );
+    content.addBodyPart(mainPart);
+    m.setContent(content);
+    m.setSubject("HtmlTeste");
+    
+    return m;
+    
+  }
+  
+  
+  public static Message HtmlTeste2(Session session)
+      throws MessagingException {
+      
+    SMTPMessage m = new SMTPMessage(session);
+    MimeMultipart content = new MimeMultipart();
+    MimeBodyPart mainPart = new MimeBodyPart();
+    mainPart.setText(
+    
+"<!DOCTYPE html>"+
+"<html>"+
+  "<head>"+
+    "<title>Foundation | Welcome</title>"+
+  "</head>"+
+    "<body>"+
+        "<style>"+
+        
+   ".progress {"+
+     "width: 30%;	 "+
+     "background-color: #F6F6F6;"+
+     "height: 1.5625rem;"+
+     "border: 1px solid white;"+
+     "padding: 0.125rem;"+
+     "margin-bottom: 0.625rem; }"+
+     ".progress .meter {"+
+       "background: #2ba6cb;"+
+       "height: 100%;"+
+       "display: block; }"+
+     ".progress.secondary .meter {"+
+       "background: #e9e9e9;"+
+       "height: 100%;"+
+       "display: block; }"+
+     ".progress.success .meter {"+
+       "background: #5da423;"+
+       "height: 100%;"+
+       "display: block; }"+
+     ".progress.alert .meter {"+
+       "background: #c60f13;"+
+       "height: 100%;"+
+       "display: block; }"+
+     ".progress.radius {"+
+       "border-radius: 3px; }"+
+       ".progress.radius .meter {"+
+         "border-radius: 2px; }"+
+     ".progress.round {"+
+       "border-radius: 1000px; }"+
+       ".progress.round .meter {"+
+         "border-radius: 999px; }"+
+        
+        "p.intro{"+
+        "color: green;"+
+        "}"+  
+        
+        "</style>"+
+        
+        "<div class=\"round progress large-10\">"+
+          "<span class=\"meter\" style=\"width: 10%\"></span>"+
+        "</div>"+
+
+        "<p class=\"intro\">Oi tudo bem</p>"+
+        
+        "</body>"+
+      "</html>"  , "US-ASCII", "html"
+            
+    );
+    content.addBodyPart(mainPart);
+    m.setContent(content);
+    m.setSubject("HtmlTeste2");
+    
+    return m;
+    
+  }
+
+  
+  public static Message HtmlTeste3(Session session)
+      throws MessagingException {
+      
+    SMTPMessage m = new SMTPMessage(session);
+    MimeMultipart content = new MimeMultipart();
+    MimeBodyPart mainPart = new MimeBodyPart();
+    mainPart.setText(
+    
+
+    "<!DOCTYPE html>"+
+"<html lang=\"en\">"+
+  "  <head>"+
+   "     <meta charset=\"utf-8\">"+
+
+    "    <title>Simple HTML+CSS progress bar</title>"+
+     "   <style>"+
+      "      body {"+
+               
+       "         font-family: Arial, sans-serif;"+
+       "         padding: 0 10%;"+
+        "        background-color: #eaeaea;"+
+       "     }"+
+        "    .graph {"+
+         "       width: 500px; "+
+          "      height: 30px;"+
+          "      border: 1px solid #888; "+
+          "      background: rgb(168,168,168);"+
+          "      background: -moz-linear-gradient(top, rgba(168,168,168,1) 0%, rgba(204,204,204,1) 23%);"+
+          "      background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(168,168,168,1)), color-stop(23%,rgba(204,204,204,1)));"+
+          "      background: -webkit-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+          "      background: -o-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+          "      background: -ms-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+          "      filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a8a8a8', endColorstr='#cccccc',GradientType=0 );"+
+          "      background: linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+          "      position: relative;"+
+          "  }"+
+          "  #bar {"+
+          "      height: 29px; "+
+          "      background: rgb(255,197,120); "+
+          "      background: -moz-linear-gradient(top, rgba(255,197,120,1) 0%, rgba(244,128,38,1) 100%); "+
+          "      background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,197,120,1)), color-stop(100%,rgba(244,128,38,1))); "+
+          "      background: -webkit-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+          "      background: -o-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+          "      background: -ms-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+          "      background: linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+          "      border-top: 1px solid #fceabb;"+
+          "  }"+
+          "  #bar p { position: absolute; text-align: center; width: 100%; margin: 0; line-height: 30px; }"+
+          "  .error {"+
+          
+          "      background-color: #fceabb;"+
+          "      padding: 1em;"+
+          "      font-weight: bold;"+
+          "      color: red;"+
+          "      border: 1px solid red;"+
+          "  }"+
+        "</style>"+
+
+    "</head>"+
+    "<body>"+
+    "    <h1>Simple HTML+CSS progress bar</h1>"+
+    "    <p>You could animate this, but it was intended as a progress indicator through a multi-page form process.</p>"+
+    "    <h2>HTML5 <code>progress</code> element with HTML/CSS fallback</h2>"+
+    "       <progress value=\"34\" max=\"100\"><div id=\"progress\" class=\"graph\"><div id=\"bar\" style=\"width:34%\"><p>34% complete</p></div></div></progress>"+
+    "       <p><em>(Special thanks to Krijn Hoetmer)</em></p>"+
+    "    <h2>Just the HTML/CSS fallback (a <code>div</code> containing another <code>div</code>)</h2>"+
+    "    <div id=\"progress\" class=\"graph\"><div id=\"bar\" style=\"width:60%\"><p>60% complete</p></div></div>"+
+    "</body>"+
+"</html>        " , "US-ASCII", "html"
+            
+            
+    );
+    content.addBodyPart(mainPart);
+    m.setContent(content);
+    m.setSubject("HtmlTeste3");
+    
+    return m;
+    
+  }
+  
+  public static Message HtmlTeste4(Session session)
+      throws MessagingException {
+      
+    SMTPMessage m = new SMTPMessage(session);
+    MimeMultipart content = new MimeMultipart();
+    MimeBodyPart mainPart = new MimeBodyPart();
+    mainPart.setText(
+    
+"<!DOCTYPE html>"+
+"<html lang=\"en\">"+
+    "<head>"+
+        "<meta charset=\"utf-8\">"+
+        "<title>Simple HTML+CSS progress bar</title>"+
+        "<style>"+
+        "</style>"+
+    "</head>"+
+    "<body>"+
+
+"<h2>Just the HTML/CSS fallback (a <code>div</code> containing another <code>div</code>)</h2>"+
+
+"<div id=\"progress\"  style=\"width: 500px; "+
+                "height: 30px;"+
+                "border: 1px solid #888; "+
+                "background: rgb(168,168,168);"+
+                "background: -moz-linear-gradient(top, rgba(168,168,168,1) 0%, rgba(204,204,204,1) 23%);"+
+                "background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(168,168,168,1)), color-stop(23%,rgba(204,204,204,1)));"+
+                "background: -webkit-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+                "background: -o-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+                "background: -ms-linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%);"+
+                "filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a8a8a8', endColorstr='#cccccc',GradientType=0 );"+
+                "background: linear-gradient(top, rgba(168,168,168,1) 0%,rgba(204,204,204,1) 23%); "+
+                "position: relative;  \"><div id=\"bar\" style=\"height: 29px; "+
+                "background: rgb(255,197,120); "+
+                "background: -moz-linear-gradient(top, rgba(255,197,120,1) 0%, rgba(244,128,38,1) 100%); "+
+                "background: -webkit-gradient(linear, left top, left bottom, color-stop(0%,rgba(255,197,120,1)), color-stop(100%,rgba(244,128,38,1))); "+
+                "background: -webkit-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+                "background: -o-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+                "background: -ms-linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+                "background: linear-gradient(top, rgba(255,197,120,1) 0%,rgba(244,128,38,1) 100%); "+
+                "border-top: 1px solid #fceabb; width:1%\"><p style=\"font-family: Arial, sans-serif;font-weight: bold;position: absolute; text-align: center; width: 100%; margin: 0; line-height: 30px;\"   >1%</p></div></div>"+
+
+       " 100%(completo)"+
+        
+    "</body>"+
+"</html>" , "US-ASCII", "html"
+            
+    );
+    content.addBodyPart(mainPart);
+    m.setContent(content);
+    m.setSubject("HtmlTeste4");
+    
+    return m;
+    
+  }
+  
   
   //
   //
